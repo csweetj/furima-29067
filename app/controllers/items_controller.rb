84f99637b7406e1a0d415052a45b0f40b056dbe9
csweetj <csweetj_9636@yahoo.co.jp>
2020-09-13 11:07:27 +0900

    リダイレクトアクションて定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show,]
 
-  def index   
+  def index
   end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
+private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
