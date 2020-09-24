e00f1247ff4f0232f58bca05faaf02c4ef76ee07
csweetj <csweetj_9636@yahoo.co.jp>
2020-09-24 22:19:02 +0900

    テストコード修正
    @itemのデータをFactoryBotへ統合、
    テスト項目を単体実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,11 @@ FactoryBot.define do
     name                {Faker::Name.initials}
     describe {Faker::Lorem.sentence}
     price   {Faker::Number.number(digits: 4)}
+    category_id {2}
+    condition_id {2}
+    delivery_day_id {2}
+    delivery_fee_id {2}
+    prefecture_id {2}
     association :user 
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,11 +4,6 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
     @item.image = fixture_file_upload('public/image/Railsの処理の流れ_0409.png')
-    @item.category_id = 2
-    @item.condition_id = 2
-    @item.delivery_day_id = 2
-    @item.delivery_fee_id = 2
-    @item.prefecture_id = 2
   end
 
   describe '商品の出品' do
@@ -20,36 +15,58 @@ RSpec.describe Item, type: :model do
   end
  
   context "商品を出品できない場合" do
-    it "商品名、画像、商品説明、値段がないと商品を出品できない" do
+    it "商品名が空だと商品を出品できない" do
       @item.name = ""
-      @item.image = nil
-      @item.describe = ""
-      @item.price = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-      expect(@item.errors.full_messages).to include("Describe can't be blank")
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-      expect(@item.errors.full_messages).to include("Price must be number")
-      expect(@item.errors.full_messages).to include("Price between ¥300〜¥9,999,999")
     end     
-    it "カテゴリー、商品の状態、配送料、発送元、発送日数が未選択だと出品できない" do
-      @item.category_id = 1
-      @item.condition_id = 1
-      @item.delivery_day_id = 1
-      @item.delivery_fee_id = 1
-      @item.prefecture_id = 1
+    it "画像が空だと商品を出品できない" do
+      @item.image = nil
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-      expect(@item.errors.full_messages).to include("Condition can't be blank")
-      expect(@item.errors.full_messages).to include("Delivery day can't be blank")
-      expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
-      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-    end
-    it "値段が¥300〜¥9,999,999以外と入力された場合商品を出品できない" do
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end     
+    it "商品説明が空だと商品を出品できない" do
+      @item.describe = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Describe can't be blank")
+      
+    end 
+    it "値段が空だと商品を出品できない" do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+      expect(@item.errors.full_messages).to include("Price between ¥300〜¥9,999,999")
+      expect(@item.errors.full_messages).to include("Price must be number")
+    end       
+    it "値段が¥300〜¥9,999,999以外を入力した場合商品を出品できない" do
       @item.price = "aaa"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price between ¥300〜¥9,999,999")
+    end
+    it "カテゴリーが未選択だと出品できない" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it "商品状態が未選択だと出品できない" do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+    it "配達日が未選択だと出品できない" do
+      @item.delivery_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+    end
+    it "運賃が未選択だと出品できない" do
+      @item.delivery_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+    end
+    it "出荷元が未選択だと出品できない" do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
     it "ユーザーが紐付いていないとツイートは保存できない" do
       @item.user = nil
