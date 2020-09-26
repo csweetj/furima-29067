574732f0ad4b90dd837bea42b99ea6830011ffdf
csweetj <csweetj_9636@yahoo.co.jp>
2020-09-26 12:10:14 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show,]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -19,6 +23,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
   def item_params
     params.require(:item).permit(:name, :image, :describe, :price, :category_id, :condition_id, :delivery_day_id, :delivery_fee_id, :prefecture_id).merge(user_id: current_user.id)
