a18d262e214cdfee25dad6f4b93d0670566299f6
csweetj <csweetj_9636@yahoo.co.jp>
2020-10-04 20:02:31 +0900

    Revert "決済機能のサーバーサイト実装"
    This reverts commit 858495655696f8e1edadfdc6bd8ad0fc8b40c720.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
+    @item = Item.find_by(params[:id])
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      pay_item
       @order_address.save
       return redirect_to root_path
     else
-      render "index"
+      @item = Item.find_by(params[:id])
+      render action: :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id])
-  end
-
-  def pay_item
-    @item = Item.find_by(params[:id])
-    Payjp.api_key = "" 
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],   
-        currency: 'jpy'              
-      )
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id])
   end
 end
