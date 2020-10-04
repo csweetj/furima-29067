858495655696f8e1edadfdc6bd8ad0fc8b40c720
csweetj <csweetj_9636@yahoo.co.jp>
2020-10-04 19:56:25 +0900

    決済機能のサーバーサイト実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,32 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
-    @item = Item.find_by(params[:id])
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       return redirect_to root_path
     else
-      @item = Item.find_by(params[:id])
-      render action: :index
+      render "index"
     end
   end
 
   private
 
   def order_params
-    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id])
+  end
+
+  def pay_item
+    @item = Item.find_by(params[:id])
+    Payjp.api_key = "" 
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],   
+        currency: 'jpy'              
+      )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :token, :item_id
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,10 +8,11 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number
+    validates :token
     end
 
     def save
-      order = Order.create
+      Order.create(item_id: item.id, user_id: user.id)
       Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
     end
 end
