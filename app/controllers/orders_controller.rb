8d7531b00cfb279a3eaecc0049bdabd16b39f6a4
csweetj <csweetj_9636@yahoo.co.jp>
2020-10-04 18:38:30 +0900

    決済機能のサーバーサイド実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
+    @item = Item.find_by(params[:id])
   end
 
   def create
@@ -9,13 +10,14 @@ class OrdersController < ApplicationController
       @order_address.save
       return redirect_to root_path
     else
-      render "index"
+      @item = Item.find_by(params[:id])
+      render action: :index
     end
   end
 
   private
 
   def order_params
-    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number)
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id])
   end
 end
