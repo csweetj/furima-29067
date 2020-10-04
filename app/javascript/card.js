85b0c83097a1c286b7fe1469a6a9d3d5962b4dc4
csweetj <csweetj_9636@yahoo.co.jp>
2020-10-04 20:08:00 +0900

    Revert "Update card.js"
    This reverts commit 910a4e889766b6bed98db60bdaadafe1918bb3d5.

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(""); //非公開テストキー
+  Payjp.setPublicKey("");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
