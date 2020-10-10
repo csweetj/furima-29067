746f126c7d2aae891a70e1122be973cc3bb5d2fb
csweetj <csweetj_9636@yahoo.co.jp>
2020-10-10 15:25:43 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_new_user_session, except: [:index, :show,]
-  before_action :set_item, only: [:show, :desytroy, :edit, :update]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
